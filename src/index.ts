07c57b85d4b29cdd282330abdcc64d48c3844c18
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-10-11 23:15:18 +0500

    1115

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import './main.css';
 import * as yup from 'yup';
+const loginname:HTMLInputElement=document.querySelector("#loginparol")!;
+const loginemail:HTMLInputElement=document.querySelector("#loginemail")!;
+const loginbtn:HTMLInputElement=document.querySelector("#loginbtn")!;
+
+const register:HTMLInputElement=document.querySelector(".register")!;
+
+
+
+
