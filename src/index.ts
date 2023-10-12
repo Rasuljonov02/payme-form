7520c080b74bdfb9f194c109c2ded7b32eccf1c9
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-10-12 12:57:50 +0500

    rsf

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-import './main.css';
-import * as yup from 'yup';
-const loginname:HTMLInputElement=document.querySelector("#loginparol")!;
-const loginemail:HTMLInputElement=document.querySelector("#loginemail")!;
-const loginbtn:HTMLInputElement=document.querySelector("#loginbtn")!;
+import "./main.css";
+// import * as yup from "yup";
+import "./register.ts";
+import "./forget.ts";
 
-const register:HTMLInputElement=document.querySelector(".register")!;
+const login: HTMLParagraphElement = document.querySelector(".login")!;
+const parol: HTMLInputElement = document.querySelector("#loginparol")!;
+const email: HTMLInputElement = document.querySelector("#loginemail")!;
+const loginbtn: HTMLButtonElement = document.querySelector("#loginbtn")!;
+const register: HTMLParagraphElement = document.querySelector(".register")!;
 
+let malumotlar: { email: string; password: string }[] = [];
 
+loginbtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	const emailValue: string = email.value;
+	const parolValue: string = parol.value;
 
-
+	register.style.display = "block";
+	login.style.display = "none";
+	malumotlar.push({ email: emailValue, password: parolValue });
+	console.log(malumotlar);
+});
---
+++ src/register.ts
@@ -0,0 +1,37 @@
+const login: HTMLParagraphElement = document.querySelector(".login")!;
+const registrfirstname: HTMLInputElement = document.querySelector("#registrfirstname")!;
+const registrlasname: HTMLInputElement = document.querySelector("#registrlasname")!;
+const registrtel: HTMLInputElement = document.querySelector("#registrtel")!;
+const registremail: HTMLInputElement = document.querySelector("#registremail")!;
+const registrparol: HTMLInputElement = document.querySelector("#registrparol")!;
+const registrparoltakror: HTMLInputElement = document.querySelector("#registrparoltakror")!;
+const registrbtn: HTMLButtonElement = document.querySelector("#registrbtn")!;
+
+const register1: HTMLParagraphElement = document.querySelector(".register1")!;
+const forget1: HTMLParagraphElement = document.querySelector(".forget1")!;
+
+let malumotlar1: { name: string; lastname: string; tel: string; email: string; password: string; takrorparol: string }[] = [];
+
+registrbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const firstnameValue: string = registrfirstname.value;
+  const lastnameValue: string = registrlasname.value;
+  const telValue: string = registrtel.value;
+  const emailValue: string = registremail.value;
+  const passwordValue: string = registrparol.value;
+  const takrorparolValue: string = registrparoltakror.value;
+
+  malumotlar1.push({
+    name: firstnameValue,
+    lastname: lastnameValue,
+    tel: telValue,
+    email: emailValue,
+    password: passwordValue,
+    takrorparol: takrorparolValue,
+  });
+  console.log(malumotlar1);
+
+  register1.style.display = "none";
+  login.style.display = "none";
+  forget1.style.display = "block";
+});
